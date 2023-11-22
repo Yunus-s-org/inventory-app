e79bf48f20e7993849966fbc7f507ea992112f67
M <muzameelahmed1@gmail.com>
2023-11-22 11:18:17 +0000

    dynamic search bar

--- public/react/components/Search.js
+++ public/react/components/Search.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { IoSearchSharp } from "react-icons/io5";
 
-const Search = () => {
+const Search = ({fetchProductsByName}) => {
   return (
     <div className="links pr-5 flex justify-center items-center relative w-full">
     <input
@@ -9,6 +9,7 @@ const Search = () => {
       name="search"
       placeholder="Search for an item..."
       className="bg-[#f9fafb00] w-[50%] py-4 pl-3 rounded-full mt-5  border-2 border-[#4338ca]"
+      onChange={e => fetchProductsByName(e.target.value)}
     />
     <IoSearchSharp className="text-3xl text-[#4338ca] absolute right-[27%] top-[43%]" />
   </div>
