d92ac9b55650a55325e13bc6ee1a9a1081bd64c5
Yunusade <yunus.adebayo@sky.uk>
2023-11-22 09:32:02 +0000

    Broke down components

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { SaucesList } from "./SaucesList";
 import { IoSearchSharp } from "react-icons/io5";
-import ProductList from "./ProductList";
 
 // import and prepend the api url to any fetch calls
 import apiURL from "../api";
+import ProductList from "./ProductList";
+import Search from "./Search";
 
 export const App = () => {
   const [sauces, setSauces] = useState([]);
@@ -37,15 +38,7 @@ export const App = () => {
           All products
         </a>
       </nav>
-      <div className="links pr-5 flex justify-center items-center relative w-full">
-        <input
-          type="text"
-          name="search"
-          placeholder="Search for an item..."
-          className="bg-[#f9fafb00] w-[50%] py-4 pl-3 rounded-full mt-5  border-2 border-[#4338ca]"
-        />
-        <IoSearchSharp className="text-3xl text-[#4338ca] absolute right-[27%] top-[43%]" />
-      </div>
+      <Search />
       <ProductList />
     </main>
   );
